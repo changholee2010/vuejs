bd7cbbfe6127ec29ac06cb0271ef2adf01182639
changholee2010 <148027761+changholee2010@users.noreply.github.com>
2025-09-01 19:42:00 +0900

    Update main.js

--- board-project/src/main.js
+++ board-project/src/main.js
@@ -3,4 +3,4 @@ import App from "./App.vue";
 import router from "./router";
 
 createApp(App).use(router).mount("#app");
-window.Kakao.init("ed68cdae999a45861eb5fe143fd1276a");
+window.Kakao.init("");
